b2ae798abe45cea5db8fa764f35ea5bb4cfe547b
Matthias <matthias@waytation.com>
2017-05-12 11:27:37 +0200

    solution array-sum-pairs

--- javascript/array-sum-pairs/index.js
+++ javascript/array-sum-pairs/index.js
@@ -1,3 +1,10 @@
-module.exports = function(array, sum) {
-
+module.exports = function (array, sum) {
+  var map = {}
+  for (var i = 0, length = array.length; i < length; i++) {
+    if ((sum - array[i]) in map) {
+      return [sum - array[i], array[i]];
+    }
+    map[array[i]] = true
+  }
+  return undefined;
 };
